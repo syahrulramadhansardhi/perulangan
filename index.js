b95b3560299b870d4efcf977eac39c721e1f3496
syahrulramadhansardhi <ramadhansyahrul1386@gmail.com>
2024-11-23 12:36:11 +0700

    new

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const nomor = 1
+let lokal = 2
+let jumlahlokal = 10
+let lokalpenuh = 5
+while (lokal < jumlahlokal) {
+    console.log('lokal' + jumlahlokal + 'lokal penuh')
+    lokal++
+}
+
+for(let lokalNo = 4; lokalNo <= jumlahlokal; lokalNo++){
+    console.log('lokal' + lokalNo + 'kosong')
+}
